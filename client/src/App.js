61baa029fd050d76bb343dcd91891370561431e0
Ben Leone <benleone90@users.noreply.github.com>
2019-05-13 22:33:44 -0400

    working page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Jumbotron from "./components/Jumbotron";
 function App() {
   return (
     <Router>
       <div>
-        <Nav />
+        <Jumbotron />
       </div>
     </Router>
   );
